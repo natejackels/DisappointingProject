27a497b49a85425e3d4e6b73074624f4481a01e1
Nathan Jackels <natejackels@gmail.com>
2014-02-15 00:19:22 -0500

    Added documentation for class and methods.

--- Sprint 1/Robot/Robot.java
+++ Sprint 1/Robot/Robot.java
@@ -2,14 +2,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ *	Class: Robot
+ *	@author Nathan Jackels
+ *	Description: The only seen by the application's control class. All Keyboard/Mouse/App work is delegated through this class by Control.
+ */
 public class Robot {
-	Process vlc = null;
-	String vlcFolder = "";
-	Keyboard keyboard;
-	Mouse mouse;
-	Control control;
+	private Process vlc = null;
+	private String vlcFolder = "";
+	private Keyboard keyboard;
+	private Mouse mouse;
+	private Control control;
 	
-	
+	/**
+	 *	Method: Robot(Control control)
+	 *	@author Nathan Jackels
+	 *	@param control The reference to control so that packets can be sent back to control.
+	 *	@see TSP/Control/Robot_Packet_Design
+	 *  Description: The constructor for the class
+	 */
 	public Robot(Control control){
 		//TODO
 		this.control = control;
@@ -17,18 +28,49 @@ public class Robot {
 		//Create Mouse
 	}
 	
+	/**
+	 *	Method: sendPacket(RobotPacket e)
+	 *	@author Nathan Jackels
+	 *	@param e The RobotPacket that will be interpreted by the Robot class
+	 *  Description: The method used by Control to delegate work to the Robot class
+	 */
 	public void sendPacket(RobotPacket e){
 		interpret(e.getApplication(), e.getEvent(), e.getInfo());
 	}
 	
+	/**
+	 *	Method: sendPacket(String app, String event, String arg)
+	 *	@author Nathan Jackels
+	 *	@param app The application being referenced in the packet that will be sent
+	 *	@param event The event that will be used in the packet that will be sent
+	 *	@param arg The argument that will be added to the packet that will be sent
+	 *	Description: A helper method to send a packet to control.
+	 */
 	private void sendPacket(String app, String event, String arg){
 		String[] e = {arg};
 		control.sendRobotPacket(new RobotPacket(app, event, e));
 	}
+	
+	/**
+	 *	Method: sendPacket(String app, String event, String[] args)
+	 *	@author Nathan Jackels
+	 *	@param app The application being referenced in the packet that will be sent
+	 *	@param event The event that will be used in the packet that will be sent
+	 *	@param args The array of arguments that will be used in the packet sent to control
+	 *	Description: A helper method to send a packet to control.
+	 */
 	private void sendPacket(String app, String event, String[] args){
 		control.sendRobotPacket(new RobotPacket(app, event,args));
 	}
 	
+	/**
+	 *	Method: interpret(String prgm, String cmd, String[] args)
+	 *	@author Nathan Jackels
+	 *	@param prgm The relevant program that the packet was sent for.
+	 *	@param cmd The command for the program.
+	 *	@param args An array of arguments for interpretation in relation to the program and command.
+	 *	Description: A method that sends information from the packets to relevent methods within the class.
+	 */
 	private void interpret(String prgm, String cmd, String[] args){
 		if((prgm == null) || (prgm.length() == 0) || (cmd == null) || (cmd.length() == 0)){
 			sendPacket("Robot", "BadPacket", null);
@@ -39,6 +81,13 @@ public class Robot {
 		}
 	}
 	
+	/**
+	 *	Method: vlc(String cmd, String[] args)
+	 *	@author Nathan Jackels
+	 *	@param cmd The command for the VLC program.
+	 *	@param args An array of arguments for interpretation in relation to the command for vlc.
+	 *	Description: A method that interprets commands for the VLC program and carries them out or delegates them further if necessary.
+	 */
 	private void vlc(String cmd, String[] args){
 		//TODO: Check Process List for VLC
 		switch(cmd){
