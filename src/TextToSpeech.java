4740026cfdf29a6f07ad1b67ef890944b9106752
thekakester <mitcheld@mtu.edu>
2014-03-05 15:54:56 -0500

    Implemented MaryTTS fully

--- src/TextToSpeech.java
+++ src/TextToSpeech.java
@@ -1,63 +1,32 @@
-import marytts.client.MaryClient;
-import marytts.client.MaryGUIClient;
-import marytts.server.Mary;
-import marytts.server.MaryServer;
-import marytts.util.http.Address;
+import java.util.Set;
 
-import com.sun.speech.freetts.*;
+import javax.sound.sampled.AudioInputStream;
+
+import marytts.LocalMaryInterface;
+import marytts.MaryInterface;
+import marytts.exceptions.SynthesisException;
+import marytts.util.data.audio.AudioPlayer;
 /*If the above line is giving you an error, that is because
  * you need to add "lib/freetts.jar" to your classpath
  */
 public class TextToSpeech {
 	public Controller parent;
-	private Voice voice;
+	private MaryInterface marytts;
 	
 	public TextToSpeech(Controller w){
 		parent = w;
-		//Load up the freeTTS voice manager
-		
-		//Start the server for tts
-		/*Mary mary = new Mary();
 		try {
-			mary.main(new String[0]);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		/*MaryServer server = new MaryServer();
-		server.run();*/
-		//server.run();
-		
-		//Start the client
-		/*MaryGUIClient client;
-		try {
-			client = new MaryGUIClient();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}*/
-		
-		String path = System.getProperty("user.dir")+"\\src\\mbrola";
-		System.out.println("Searching " + path + " for voices");
-		System.setProperty("mbrola.base", path);
-		
-		VoiceManager voiceManager = VoiceManager.getInstance();
-		
-		Voice[] voices = voiceManager.getVoices();
-
-		//I like kevin16, so we will use that for right now
-		for (int i = 0; i < voices.length; i++) {
-			System.out.println("Loaded Voice: " + voices[i].getName());
-			if (voices[i].getName().equals("mbrola_us1")) {
-				voice = voices[i];
-				
-				//Preapare the voice
-				voice.allocate();
+			marytts = new LocalMaryInterface();
+			Set<String> voices = marytts.getAvailableVoices();
+			for (String s : voices) {
+				marytts.setVoice(s);
+				System.out.println("Loaded voice for MaryTTS");
+				break;
 			}
-		}
-		
-		//say("Voices are working for the most part");*/
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 	
 	/**Send a packet to TTS.  The message of the packet will be spoken
@@ -73,7 +42,16 @@ public class TextToSpeech {
 	 * @param s the string to say
 	 */
 	private void say(String s) {
-		voice.speak(s);
+		try {
+			AudioInputStream audio = marytts.generateAudio(s);
+			AudioPlayer player = new AudioPlayer(audio);
+			player.start();
+			player.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
