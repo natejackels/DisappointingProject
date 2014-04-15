3f2ee6080131cd4f5812133482b0c021d6dfd6c9
fiffer <njfisher@mtu.edu>
2014-04-15 17:03:11 -0400

    Small GUI changes

--- src/GUI.java
+++ src/GUI.java
@@ -8,11 +8,19 @@ public class GUI extends javax.swing.JFrame {
     // removed boolean recording
     public Controller parent;
     String commandToSend = "";  //String to be passed to control after typed in
+    boolean debugMode = true;
 
     public GUI(Controller W) {
         parent = W;
+        
         initComponents();
         this.setVisible(true);
+         if (debugMode == false){
+             debugBorder.setVisible(false);
+             debugLabel.setVisible(false);
+             debugLabelOutput.setVisible(false);
+    }
+        
     }
 
    
@@ -48,6 +56,7 @@ public class GUI extends javax.swing.JFrame {
         commandField = new javax.swing.JTextField();
         sendButton = new javax.swing.JButton();
         recordToggleButton = new javax.swing.JToggleButton();
+        settingsJB = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -88,7 +97,7 @@ public class GUI extends javax.swing.JFrame {
         mainBorder.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
         titleHeader.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        titleHeader.setText("Old People Helper 3000");
+        titleHeader.setText("Empower");
 
         inputText.setText("Input Command");
 
@@ -123,13 +132,14 @@ public class GUI extends javax.swing.JFrame {
                 .addGroup(mainBorderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(mainBorderLayout.createSequentialGroup()
                         .addComponent(recordToggleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(0, 90, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainBorderLayout.createSequentialGroup()
-                        .addGroup(mainBorderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(mainBorderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(commandField)
                             .addGroup(mainBorderLayout.createSequentialGroup()
-                                .addGap(0, 28, Short.MAX_VALUE)
-                                .addComponent(titleHeader))
-                            .addComponent(commandField))
+                                .addGap(67, 67, 67)
+                                .addComponent(titleHeader)
+                                .addGap(0, 0, Short.MAX_VALUE)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(sendButton)))
                 .addContainerGap())
@@ -149,12 +159,21 @@ public class GUI extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        settingsJB.setText("Settings");
+        settingsJB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                settingsJBActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(settingsJB)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(exitButton)
                 .addContainerGap())
             .addComponent(debugBorder, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -165,7 +184,9 @@ public class GUI extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(mainBorder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(exitButton)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(exitButton)
+                    .addComponent(settingsJB))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(debugBorder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -220,6 +241,10 @@ public class GUI extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_exitButtonActionPerformed
 
+    private void settingsJBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_settingsJBActionPerformed
+        parent.sendPacket(new GUIPacket("settings"));
+    }//GEN-LAST:event_settingsJBActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField commandField;
     private javax.swing.JPanel debugBorder;
@@ -230,6 +255,7 @@ public class GUI extends javax.swing.JFrame {
     private javax.swing.JPanel mainBorder;
     private javax.swing.JToggleButton recordToggleButton;
     private javax.swing.JButton sendButton;
+    private javax.swing.JButton settingsJB;
     private javax.swing.JLabel titleHeader;
     // End of variables declaration//GEN-END:variables
 }
